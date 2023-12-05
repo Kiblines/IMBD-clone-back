64e92ce463356318930ad0466d0c0d2dd5c84529
Anthony Lines <linesanthony018@gmail.com>
2023-12-05 10:30:54 +0100

    add nodemon

--- server.js
+++ server.js
@@ -8,12 +8,7 @@ app.use(express.json());
 const mongoDBUri = "mongodb://localhost:27017/IMBD";
 
 mongoose
-  .connect(mongoDBUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
+  .connect(mongoDBUri)
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch((error) => console.error("Connexion à MongoDB échouée !", error));
 
@@ -21,4 +16,7 @@ mongoose
 const port = 3000;
 app.listen(port, () => {
   console.log(`Serveur démarré sur http://localhost:${port}`);
+});
+app.get("/", (req, res) => {
+  res.send("Bienvenue sur mon API de films IMDB!");
 });
